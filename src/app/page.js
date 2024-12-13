9b20a9fd09144dc1798fa8c34f8888e1ed7d6936
REINNn0820 <nymaa4764@gmail.com>
2024-12-13 23:53:33 +0800

    lastdance

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,4 @@
 "use client";
-// import '@fortawesome/fontawesome-free/css/all.min.css';
-// import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { useState, useEffect } from "react";
 import {
   Circle,
@@ -9,13 +7,24 @@ import {
   WhiteSquare,
   MidCircle,
   CircleGray,
+  Icons,
 } from "./components/component";
+
+
 
 const API_KEY = "a9bd50909a544a9c84172455241312";
 
 export default function Home() {
   const [search, setSearch] = useState("");
   const [city, setCity] = useState("");
+  const [dayweather,setDayWeather] = useState({
+    number:0 ,
+    condition:'',
+  })
+  const [nightweather,setNightWeather] = useState({
+    nothing:0 ,
+    condition:'',
+  })
   const onChangeText = (e) => {
     setSearch(e.target.value);
   };
@@ -27,17 +36,24 @@ export default function Home() {
   };
   useEffect(() => {
     fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&aqi=no`
+      `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=1&aqi=no&alerts=no`
     )
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.forecast.forecast.day[0].day.maxtemp_c);
+        setDayWeather({
+          number:(data.forecast.forecastday[0].day.maxtemp_c),
+          condition:(data.forecast.forecastday[0].day.condition.text)
+        })
+        setNightWeather({
+          nothing:(data.forecast.forecastday[0].day.mintemp_c),
+          condition:(data.forecast.forecastday[0].day.condition.text)
+        })   
       });
-  }, []);
+  }, [city]);
   return (
     <div className="h-screen w-full flex justify-center overflow-hidden">
       <div className="bg-stone-50 w-full h-[1200px] flex items-center flex-col-reverse justify-between pb-[200px] pt-[50px] rounded-l-[42px]">
-        <Card value="day" city={city} />
+        <Card value="day" city={city} number={dayweather.number} condition={dayweather.condition}/>
         <Searchinput
           search={search}
           onChangeText={onChangeText}
@@ -57,43 +73,46 @@ export default function Home() {
         <CircleGray size={1340} top={-120} left={-650} />
         <Circle size={1740} top={-300} left={-880} />
         <CircleGray size={1740} top={-300} left={-880} />
-        <Card value="night" city={city} />
+        <Card value="night" city={city} nothing={nightweather.nothing} condition={dayweather.condition} />
         <Square size={100} top={326} left={-0} />
         <Square size={100} top={574} left={-0} />
         <WhiteSquare size={40} top={390} left={-12} />
         <WhiteSquare size={40} top={566} left={-12} />
+        <img src="leftlogo.png" className="absolute top-[450px] left-[-50px]"/>
+        <img src="rightlogo (2).png" className="absolute top-[450px] left-[10px]"/>
+        <img/>
       </div>
     </div>
   );
 }
 
-const Card = ({ value, city }) => {
+const Card = ({ value, city, number,nothing,condition}) => {
   const cardBackgroundDay = "bg-[#FFFFFF]";
   const sun = "sun.png";
-  const dayTemperature = 24;
-  const nightTemperature = 17;
 
-  const temperature = value === "day" ? dayTemperature : nightTemperature;
+  const temperature = value === "day" ? number: nothing;
   const zurag = value === "day" ? sun : "moon.png";
   const cardBg = value === "day" ? cardBackgroundDay : "bg-[#111827BF]";
   const nightCardColors =
     "bg-[111827bf] bg-gradient-to-b from-[#1F2937] to-[#11182700] text-white shadow-[#111827]";
 
+  const textStyle = value === "day" ? "text-[#FF8E27] font-bold text-[24px]" : "text-[#777CCE] font-bold text-[24px]"
+
   const colors = value === "day" ? "bg-[#FFFFFFBF]" : nightCardColors;
   const temperatureStyle =
     value === "day"
-      ? "text-[144px] text-transparent bg-clip-text bg-gradient-to-b from-[#111827] to-[#6b7280] font-extrabold"
-      : "text-[144px] text-transparent bg-clip-text bg-gradient-to-b from-[#F9FAFB] to-[#F9FAFB00] font-extrabold";
+      ? "text-[96px] text-transparent bg-clip-text bg-gradient-to-b from-[#111827] to-[#6b7280] font-extrabold"
+      : "text-[96px] text-transparent bg-clip-text bg-gradient-to-b from-[#F9FAFB] to-[#F9FAFB00] font-extrabold";
 
   return (
     <div
-      className={`w-[414px] h-[832px] rounded-[48px] flex justify-center ${cardBg} z-10  overflow-hidden`}
+      className={`w-[414px] h-[832px] rounded-[48px] justify-center ${cardBg} z-10  overflow-hidden`}
     >
-      <div className={`w-[398px] h-[504px] rounded-[42px] mt-[10px] ${colors}`}>
+      <div className={`w-[398px] h-[504px] rounded-[42px] mt-[10px] ml-[8px] ${colors}`}>
         <div className="flex justify-center">
           <div className="w-[290px] mt-[64px] ml-[48px]">
             <p className="text-md">September 10, 2024</p>
-            <h2 className="text-4xl font-bold">{city}</h2>
+            <h2 className="text-4xl font-extrabold">{city}</h2>
           </div>
           <div className="mt-[93px] w-[92px]">
             <img src="localization_icon.png" alt="localization" />
@@ -105,7 +124,12 @@ const Card = ({ value, city }) => {
         <div className="mt-[30px] ml-[35px]">
           <p className={temperatureStyle}>{temperature}°</p>
         </div>
+        
       </div>
+      <div className="mt-[30px] ml-[50px]"><p className={textStyle}>{condition}</p></div>
+      <div className="flex justify-center items-center mt-36 gap-20">
+          <Icons value={value} />
+        </div>
     </div>
   );
 };
