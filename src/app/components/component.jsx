4c3a28aafb8b1921a0c0d80b92cca90aa00c7cb4
REINNn0820 <nymaa4764@gmail.com>
2024-12-13 18:23:54 +0800

    effect

---
+++ src/app/components/component.jsx
@@ -0,0 +1,81 @@
+export function Searchinput({ search, onChangeText, onPressEnter }) {
+  return (
+    <div className="w-[520px] h-[80px] bg-[#ffffff] rounded-[48px] flex  items-center gap-[24px] z-30 ">
+      <img className="opacity-20 ml-[24px]" src="Vector.png" />
+      <input
+        type="search"
+        placeholder="Search city..."
+        className="focus:outline-none border-none h-[60px] w-[350px]"
+        value={search}
+        onChange={onChangeText}
+        onKeyDown={onPressEnter}
+      ></input>
+    </div>
+  );
+}
+export function Circle({ size, top, left }) {
+  return (
+    <div
+      style={{
+        width: `${size}px`,
+        height: `${size}px`,
+        top: `${top}px`,
+        left: `${left}px`,
+      }}
+      className="border rounded-full border-[#ffffff] border-opacity-[8%] absolute z-20"
+    ></div>
+  );
+}
+export function CircleGray({ size, top, left }) {
+  return (
+    <div
+      style={{
+        width: `${size}px`,
+        height: `${size}px`,
+        top: `${top}px`,
+        left: `${left}px`,
+      }}
+      className="border rounded-full border-[#111827] border-opacity-[8%] absolute z-20"
+    ></div>
+  );
+}
+export function Square({ size, top, left }) {
+  return (
+    <div
+      style={{
+        width: `${size}px`,
+        height: `${size}px`,
+        top: `${top}px`,
+        left: `${left}px`,
+      }}
+      className="bg-[#0F141E] z-10 rounded-[36px] absolute"
+    ></div>
+  );
+}
+export function WhiteSquare({ size, top, left }) {
+  return (
+    <div
+      style={{
+        width: `${size}px`,
+        height: `${size}px`,
+        top: `${top}px`,
+        left: `${left}px`,
+      }}
+      className="bg-stone-50 absolute"
+    ></div>
+  );
+}
+
+export function MidCircle({ size, top, left }) {
+  return (
+    <div
+      style={{
+        width: `${size}px`,
+        height: `${size}px`,
+        top: `${top}px`,
+        left: `${left}px`,
+      }}
+      className="bg-stone-50 rounded-full absolute"
+    ></div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,78 @@
 "use client";
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import { faHome } from '@fortawesome/free-solid-svg-icons';
-import { Circle, Searchinput, Square, WhiteSquare } from "./components/searchinput";
+// import '@fortawesome/fontawesome-free/css/all.min.css';
+// import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { useState, useEffect } from "react";
+import {
+  Circle,
+  Searchinput,
+  Square,
+  WhiteSquare,
+  MidCircle,
+  CircleGray,
+} from "./components/component";
+
+const API_KEY = "a9bd50909a544a9c84172455241312";
 
 export default function Home() {
+  const [search, setSearch] = useState("");
+  const [city, setCity] = useState("");
+  const onChangeText = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const onPressEnter = (e) => {
+    if (e.code === "Enter") {
+      setCity(search);
+    }
+  };
+  useEffect(() => {
+    fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&aqi=no`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.forecast.forecast.day[0].day.maxtemp_c);
+      });
+  }, []);
   return (
-    <div className="h-screen w-full flex">
-      <div className="w-1/2 h-screen bg-[#F3F4F6] flex flex-col-reverse justify-between items-center  rounded-l-[42px] pb-[80px] pt-[36px]">
-        <Card value="day" />
-        <Searchinput />
+    <div className="h-screen w-full flex justify-center overflow-hidden">
+      <div className="bg-stone-50 w-full h-[1200px] flex items-center flex-col-reverse justify-between pb-[200px] pt-[50px] rounded-l-[42px]">
+        <Card value="day" city={city} />
+        <Searchinput
+          search={search}
+          onChangeText={onChangeText}
+          onPressEnter={onPressEnter}
+        />
       </div>
-      <div className="w-1/2 h-screen bg-[#0F141E] flex flex-col-reverse justify-between items-center rounded-r-[42px] pb-[80px] relative">
+      <div className="relative w-full h-[1200px] bg-[#0F141E] flex items-center flex-col-reverse justify-between pb-[200px]">
         {/* circle */}
-        <Circle size={140} top={430} left={-70}/>
-        <Circle size={340} top={330} left={-170}/>
-        <Circle size={540} top={230} left={-270}/>
-        <Circle size={940} top={30} left={-470}/>
-        <Circle size={1740} top={-230} left={-870}/>
-        <Circle size={3340} top={-450} left={-1670}/>
-        <Card value="night" />
-        <Square size={150} top={287} left={0}/>
-        <Square size={150} top={564} left={0}/>
-        <WhiteSquare size={100} top={331} left={0}/>
-        <WhiteSquare size={100} top={550} left={0}/>
+        <MidCircle size={160} top={420} left={-80} />
+        <Circle size={340} top={330} left={-170} />
+        <CircleGray size={340} top={330} left={-170} />
+        <Circle size={540} top={230} left={-270} />
+        <CircleGray size={540} top={230} left={-270} />
+        <Circle size={940} top={50} left={-460} />
+        <CircleGray size={940} top={50} left={-460} />
+        <Circle size={1340} top={-120} left={-650} />
+        <CircleGray size={1340} top={-120} left={-650} />
+        <Circle size={1740} top={-300} left={-880} />
+        <CircleGray size={1740} top={-300} left={-880} />
+        <Card value="night" city={city} />
+        <Square size={100} top={326} left={-0} />
+        <Square size={100} top={574} left={-0} />
+        <WhiteSquare size={40} top={390} left={-12} />
+        <WhiteSquare size={40} top={566} left={-12} />
       </div>
     </div>
   );
 }
 
-const Card = ({ value }) => {
-  const cardBackgroundDay = "bg-[#FFFFFFbf]";
+const Card = ({ value, city }) => {
+  const cardBackgroundDay = "bg-[#FFFFFF]";
   const sun = "sun.png";
   const dayTemperature = 24;
   const nightTemperature = 17;
-  
+
   const temperature = value === "day" ? dayTemperature : nightTemperature;
   const zurag = value === "day" ? sun : "moon.png";
   const cardBg = value === "day" ? cardBackgroundDay : "bg-[#111827BF]";
@@ -41,16 +80,20 @@ const Card = ({ value }) => {
     "bg-[111827bf] bg-gradient-to-b from-[#1F2937] to-[#11182700] text-white shadow-[#111827]";
 
   const colors = value === "day" ? "bg-[#FFFFFFBF]" : nightCardColors;
+  const temperatureStyle =
+    value === "day"
+      ? "text-[144px] text-transparent bg-clip-text bg-gradient-to-b from-[#111827] to-[#6b7280] font-extrabold"
+      : "text-[144px] text-transparent bg-clip-text bg-gradient-to-b from-[#F9FAFB] to-[#F9FAFB00] font-extrabold";
 
   return (
     <div
-      className={`w-[414px] h-[832px] rounded-[48px] flex justify-center ${cardBg} z-10`}
+      className={`w-[414px] h-[832px] rounded-[48px] flex justify-center ${cardBg} z-10  overflow-hidden`}
     >
       <div className={`w-[398px] h-[504px] rounded-[42px] mt-[10px] ${colors}`}>
         <div className="flex justify-center">
           <div className="w-[290px] mt-[64px] ml-[48px]">
             <p className="text-md">September 10, 2024</p>
-            <h2 className="text-4xl font-bold">Krakow</h2>
+            <h2 className="text-4xl font-bold">{city}</h2>
           </div>
           <div className="mt-[93px] w-[92px]">
             <img src="localization_icon.png" alt="localization" />
@@ -59,11 +102,10 @@ const Card = ({ value }) => {
         <div className="flex justify-center items-center mt-[30px]">
           <img className="h-[200px] w-[200px]" src={zurag} alt={value} />
         </div>
-        <div className="flex justify-center items-center mt-[30px]">
-          <p className="text-7xl text-black ">{temperature}°</p>
+        <div className="mt-[30px] ml-[35px]">
+          <p className={temperatureStyle}>{temperature}°</p>
         </div>
       </div>
     </div>
   );
 };
-
