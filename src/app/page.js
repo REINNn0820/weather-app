625dc85578accba26254623dcf29ed09b6d0f558
REINNn0820 <nymaa4764@gmail.com>
2024-12-16 18:21:35 +0800

    weather-app-day5

--- src/app/page.js
+++ src/app/page.js
@@ -14,50 +14,67 @@ const API_KEY = "a9bd50909a544a9c84172455241312";
 
 export default function Home() {
   const [search, setSearch] = useState("");
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState("Ulaanbaatar");
+  const [forecastDate, setForecastDate] = useState("");
   const [dayweather, setDayWeather] = useState({
     number: 0,
     condition: "",
   });
   const [nightweather, setNightWeather] = useState({
-    nothing: 0,
+    number: 0,
     condition: "",
   });
+
   const onChangeText = (e) => {
     setSearch(e.target.value);
   };
 
   const onPressEnter = (e) => {
-    if (e.code === "Enter") {
+    if (e.code === "Enter" && search.trim()) {
       setCity(search);
     }
   };
+
   useEffect(() => {
-    fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=1&aqi=no&alerts=no`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setDayWeather({
-          number: data?.forecast?.forecastday[0]?.day?.maxtemp_c,
-          condition: data?.forecast?.forecastday[0]?.day?.condition?.text,
+    if (city) {
+      fetch(
+        `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=1&aqi=no&alerts=no`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const forecastDay = data.forecast?.forecastday[0];
+
+          const date = new Date(forecastDay.date);
+          const formattedDate = date.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          });
+          setForecastDate(formattedDate);
+
+          setDayWeather({
+            number: Math.round(forecastDay.day.maxtemp_c),
+            condition: forecastDay.day.condition.text,
+          });
+          setNightWeather({
+            number: Math.round(forecastDay.day.mintemp_c),
+            condition: forecastDay.hour[23].condition.text,
+          });
+        })
+        .catch((error) => {
+          console.error("Error fetching weather data:", error);
         });
-        console.log(data);
-        setNightWeather({
-          nothing: data?.forecast?.forecastday[0]?.day?.mintemp_c,
-          nightCondition:
-            data?.forecast?.forecastday[0]?.hour[23]?.condition?.text,
-        });
-      });
+    }
   }, [city]);
+
   return (
     <div className="h-screen w-full flex justify-center overflow-hidden">
-      <div className="bg-stone-50 w-full h-[1200px] flex items-center flex-col-reverse justify-between pb-[200px] pt-[50px] rounded-l-[42px]">
+      <div className="bg-stone-50 w-full h-[1200px] flex items-center flex-col-reverse justify-between pb-[200px] pt-[50px] rounded-l-[50px] text-black">
         <Card
           value="day"
           city={city}
-          number={dayweather.number}
-          condition={dayweather.condition}
+          weather={dayweather}
+          forecastDate={forecastDate}
         />
         <Searchinput
           search={search}
@@ -65,8 +82,8 @@ export default function Home() {
           onPressEnter={onPressEnter}
         />
       </div>
+
       <div className="relative w-full h-[1200px] bg-[#0F141E] flex items-center flex-col-reverse justify-between pb-[200px]">
-        {/* circle */}
         <MidCircle size={160} top={420} left={-80} />
         <Circle size={340} top={330} left={-170} />
         <CircleGray size={340} top={330} left={-170} />
@@ -81,74 +98,111 @@ export default function Home() {
         <Card
           value="night"
           city={city}
-          nothing={nightweather.nothing}
-          condition={nightweather.nightCondition}
+          weather={nightweather}
+          forecastDate={forecastDate}
         />
         <Square size={100} top={326} left={-0} />
         <Square size={100} top={574} left={-0} />
         <WhiteSquare size={40} top={390} left={-12} />
         <WhiteSquare size={40} top={566} left={-12} />
         <img src="leftlogo.png" className="absolute top-[450px] left-[-50px]" />
+        <img src="rightlogo.png" className="absolute top-[450px] left-[10px]" />
         <img
-          src="rightlogo (2).png"
-          className="absolute top-[450px] left-[10px]"
+          src="Sunright.png"
+          className="absolute top-[100px] left-[-860px] z-0"
         />
-        <img />
+        <img src="Moonleft.png" className="absolute top-[930px] left-[650px]" />
       </div>
     </div>
   );
 }
 
-const Card = ({ value, city, number, nothing, condition, nightCondition }) => {
+export function Card({ value, city, weather, forecastDate }) {
   const isDay = value === "day";
-  const cardBackgroundDay = "bg-[#ffffffbf]";
-  const sun = "sun.png";
-  const allCondition = isDay ? condition : nightCondition;
+  const [dayStatus, setDayStatus] = useState("/Sun.png");
+  const [nightStatus, setNightStatus] = useState("/Moon.png");
+  const weatherStatus = isDay ? dayStatus : nightStatus;
+  const { number, condition } = weather;
 
-  const temperature = isDay ? number : nothing;
-  const zurag = isDay ? sun : "moon.png";
-  const cardBg = isDay ? cardBackgroundDay : "bg-[#111827BF]";
-  const nightCardColors =
-    "bg-[111827bf] bg-gradient-to-b from-[#1F2937] to-[#11182700] text-white shadow-[#111827]";
+  useEffect(() => {
+    if (isDay) {
+      if (condition.includes("Sunny")) {
+        setDayStatus("/Sun.png");
+      } else if (condition.includes("Overcast")) {
+        setDayStatus("/Cloud.png");
+      } else if (condition.includes("snow")) {
+        setDayStatus("/Snow.png");
+      } else if (condition.includes("rain")) {
+        setDayStatus("/Rain.png");
+      } else if (condition.includes("thunder")) {
+        setDayStatus("/Thunder.png");
+      } else if (condition.includes("wind")) {
+        setDayStatus("/Wind.png");
+      } else if (condition.includes("Mist")) {
+        setDayStatus("/Cloud.png");
+      } else if (condition.includes("Cloudy")) {
+        setDayStatus("/Cloud.png");
+      }
+    } else {
+      if (condition.includes("Clear")) {
+        setNightStatus("/Moon.png");
+      } else if (condition.includes("Overcast")) {
+        setNightStatus("/Cloudy.png");
+      } else if (condition.includes("snow")) {
+        setNightStatus("/Snowy.png");
+      } else if (condition.includes("rain")) {
+        setNightStatus("/Rainy.png");
+      } else if (condition.includes("thunder")) {
+        setNightStatus("/Thunderstorm.png");
+      } else if (condition.includes("wind")) {
+        setNightStatus("/Windy.png");
+      } else if (condition.includes("Cloudy")) {
+        setNightStatus("/Cloudy.png");
+      }
+    }
+  }, [city, condition]);
 
-  const textStyle = isDay
-    ? "text-[#FF8E27] font-bold text-[24px]"
-    : "text-[#777CCE] font-bold text-[24px]";
-
-  const colors = isDay ? "bg-[#FFFFFF]" : nightCardColors;
   const temperatureStyle = isDay
     ? "text-[96px] text-transparent bg-clip-text bg-gradient-to-b from-[#111827] to-[#6b7280] font-extrabold"
     : "text-[96px] text-transparent bg-clip-text bg-gradient-to-b from-[#F9FAFB] to-[#F9FAFB00] font-extrabold";
 
+  const conditionStyle = isDay
+    ? "text-[#FF8E27] font-bold text-[24px]"
+    : "text-[#777CCE] font-bold text-[24px]";
+
+  const cardBg = isDay ? "bg-[#FFFFFFbf]" : "bg-[#111827BF]";
+  const nightCardColors =
+    "bg-[111827bf] bg-gradient-to-b from-[#1F2937] to-[#11182700] text-white shadow-[#111827]";
+  const colors = isDay ? "bg-[#FFFFFF]" : nightCardColors;
+
   return (
     <div
-      className={`w-[414px] h-[832px] rounded-[48px] justify-center ${cardBg} z-20  overflow-hidden`}
+      className={`w-[414px] h-[832px] rounded-[48px] flex justify-center ${cardBg} z-10 overflow-hidden`}
     >
-      <div
-        className={`w-[398px] h-[504px] rounded-[42px] mt-[10px] ml-[8px] ${colors}`}
-      >
+      <div className={`w-[398px] h-[504px] rounded-[42px] mt-[10px] ${colors}`}>
         <div className="flex justify-center">
-          <div className="w-[290px] mt-[64px] ml-[48px]">
-            <p className="text-md">September 10, 2024</p>
-            <h2 className="text-4xl font-extrabold">{city}</h2>
-          </div>
-          <div className="mt-[93px] w-[92px]">
-            <img src="localization_icon.png" alt="localization" />
+          <div className="w-[290px] mt-[64px] ml-[10px]">
+            <p className="text-md">{forecastDate || "Date Unavailable"}</p>
+            <h2 className="text-5xl font-bold">{city || "Unknown"}</h2>
           </div>
         </div>
         <div className="flex justify-center items-center mt-[30px]">
-          <img className="h-[200px] w-[200px]" src={zurag} alt={value} />
+          <img
+            className="h-[300px] w-[300px]"
+            src={weatherStatus}
+            alt={value}
+          />
         </div>
-        <div className="mt-[30px] ml-[35px]">
-          <p className={temperatureStyle}>{temperature}°</p>
+        <div className="flex justify-center items-center mr-32 ">
+          <p className={temperatureStyle}>{number || "--"}°</p>
         </div>
-      </div>
-      <div className="mt-[30px] ml-[50px]">
-        <p className={textStyle}>{allCondition}</p>
-      </div>
-      <div className="flex justify-center items-center mt-36 gap-20">
-        <Icons value={value} />
+        <div className="mt-[30px] ml-[60px]">
+          <p className={conditionStyle}>{condition || "No Data"}</p>
+        </div>
+        <div className="flex justify-center items-center mt-[80px] gap-20">
+          <Icons value={value} />
+        </div>
       </div>
     </div>
   );
-};
+}
